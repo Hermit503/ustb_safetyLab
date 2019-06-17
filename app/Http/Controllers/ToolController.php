41df53c4bd6342fa0e50a46de04d9d732a9a998b
Norsl-s <1281851075@qq.com>
2019-06-17 22:48:47 +0800

    注册ajax获取单位下管理人员名单

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -7,9 +7,24 @@ use Illuminate\Http\Request;
 
 class ToolController extends Controller
 {
+    /*
+     * 注册选择单位 ajax请求获取该单位下管理员
+     * @author hzj
+     * @time 2019-06-17
+     * @
+     *
+     * */
     public function getUserRole()
     {
-        $user = User::where('unit_id','0')->has('roles')->get();
-        dd($user);
+        $allUser = array();
+        $user = User::where('unit_id', '=', '2')->get();
+        foreach ($user as $item) {
+            foreach ($item->roles as $getUser) {
+                if ($getUser->role == "校级管理员" || $getUser->role == "院级管理员") {
+                    array_push($allUser,$item->name);
+                }
+            }
+        }
+        return response()->json(array_unique($allUser));
     }
 }
