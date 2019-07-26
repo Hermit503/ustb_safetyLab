1d9313de42be1dfc99c431fba1d49e6949213607
Norsl-s <1281851075@qq.com>
2019-07-26 20:08:05 +0800

    fix 表名

--- database/migrations/2019_06_21_165250_create_equipments_table.php
+++ database/migrations/2019_06_21_165250_create_equipments_table.php
@@ -46,6 +46,6 @@ class CreateEquipmentTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('equipment');
+        Schema::dropIfExists('equipments');
     }
 }
