a841e36a3af4b7d627e284eb6050a5195ad8d7e8
lj <2601454425@qq.com>
2019-10-08 10:46:48 +0800

    添加缓存机制

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
 
 class NoticeController extends Controller
 {
@@ -157,7 +158,25 @@ class NoticeController extends Controller
         $notice->save();
 
         return "上传成功";
+    /**
+     * 获取已收到消息的人员姓名
+     * @param Request $request
+     * @return mixed
+     * @author lj
+     * @time 2019-09-10
+     */
+    public function getNoticeReceivedUsers($received_users)
+    {
+        $names = [];
+        //将0062 0061转为数组
+        $users = explode(" ", $received_users);
 
+        foreach ($users as $item) {
+            $name = User::where('user_id', $item)->get(['user_id', 'name']);
+            array_push($names, $name[0]);
+        }
+
+        return $names;
     }
 
     /**
@@ -187,16 +206,16 @@ class NoticeController extends Controller
         $retArr = array('刚刚', '秒前', '分钟前', '小时前', '天前', '月前', '年前');
 
         switch ($int) {
-            case $int == 0://刚刚
+            case $int == 0: //刚刚
                 $text = $retArr[0];
                 break;
-            case $int < 60:// 几秒前
+            case $int < 60: // 几秒前
                 $text = $int . $retArr[1];
                 break;
-            case $int < 3600://几分钟前
+            case $int < 3600: //几分钟前
                 $text = floor($int / 60) . $retArr[2];
                 break;
-            case $int < 86400://几小时前
+            case $int < 86400: //几小时前
                 $text = floor($int / 3600) . $retArr[3];
                 break;
             case $int < 2592000: //几天前
@@ -205,7 +224,7 @@ class NoticeController extends Controller
             case $int < 31536000: //几个月前
                 $text = floor($int / 2592000) . $retArr[5];
                 break;
-            default : //几年前
+            default: //几年前
                 $text = floor($int / 31536000) . $retArr[6];
         }
 
@@ -246,6 +265,15 @@ class NoticeController extends Controller
         $names = $this->getNames(json_decode($result[0]['users']));
         $result[0]['pictures'] = json_decode($result[0]['pictures']);
         $result[0]['users'] = $names;
+
+
+        $received_users = $result[0]['received_users'];
+        if ($received_users == '' || $received_users == null) {
+            $result[0]['received_users'] = [];
+        } else {
+            $result[0]['received_users'] = $this->getNoticeReceivedUsers($received_users);
+        }
+
         return $result;
     }
 
@@ -275,83 +303,90 @@ class NoticeController extends Controller
      * @return array
      * @author lj
      * @time 2019-8-25
+     * 添加缓存 有效期一天
+     * @author lj
+     * @time 2019-09-15
      */
     public function getAllMessage(Request $request)
     {
-        //最终结果
-        $result = [];
+        $cacheName = "allMessage".$request->user_id;
+        if ($value = Cache::get($cacheName)) {
+            return $value;
+        } else {
+            //最终结果
+            $result = [];
 
-        //未处理
-        $chemicalList = ChemicalsNotice::where([
-            ["user_id_2",$request->user_id],
-            ["isConfirm_2","0"],
-            ["receive","0"]
-        ])->get();
-        foreach ($chemicalList as $item){
-            $item['noticeType'] = "chemical";
-            array_push($result,$item);
-        }
+            //未处理
+            $chemicalList = ChemicalsNotice::where([
+                ["user_id_2", $request->user_id],
+                ["isConfirm_2", "0"],
+                ["receive", "0"]
+            ])->get();
+            foreach ($chemicalList as $item) {
+                $item['noticeType'] = "chemical";
+                array_push($result, $item);
+            }
 
-        //1已处理2被驳回
-        $chemicalDisagreeList = ChemicalsNotice::where([
-            ["user_id_1",$request->user_id],
-            ["isConfirm_2","0"],
-            ["receive","1"]
-        ])->get();
-        foreach ($chemicalDisagreeList as $item){
-            $item['noticeType'] = "chemical";
-            array_push($result,$item);
-        }
+            //1已处理2被驳回
+            $chemicalDisagreeList = ChemicalsNotice::where([
+                ["user_id_1", $request->user_id],
+                ["isConfirm_2", "0"],
+                ["receive", "1"]
+            ])->get();
+            foreach ($chemicalDisagreeList as $item) {
+                $item['noticeType'] = "chemical";
+                array_push($result, $item);
+            }
 
-        $tmplist = Notice::all();
-        $users = [];
-        $noticeList = [];
-        foreach ($tmplist as $item){
-            $users[$item['id']] = json_decode($item['users']);
-        }
-        //遍历users
-        $count = 0;
-        foreach( $users as $key=>$user ){
-            for($i = 0 ; $i < count($user) ; $i++){
-                if($user[$i] == $request->user_id){
-                    $tmp = Notice::where('id',$key)->get();
-                    //已收到信息的人
-                    $received_users = explode(" ", $tmp[0]['received_users']);
-                    $tmpName = User::where('user_id',$tmp[0]['build_id'])->get('name')->first();
-                    $tmp[0]['name'] = $tmpName['name'];
-                    $tmp[0]['noticeType'] = "notice";
-                    $tmp_received = [];
-                    foreach($received_users as $key=>$received_user){
-                        //如果相等的话表示已提交，不push
-                        if($received_user != $request->user_id){
-                            array_push($tmp_received,$received_user);
-                        }else{
-
+            $tmplist = Notice::all();
+            $users = [];
+            $noticeList = [];
+            foreach ($tmplist as $item) {
+                $users[$item['id']] = json_decode($item['users']);
+            }
+            //遍历users
+            $count = 0;
+            foreach ($users as $key => $user) {
+                for ($i = 0; $i < count($user); $i++) {
+                    if ($user[$i] == $request->user_id) {
+                        $tmp = Notice::where('id', $key)->get();
+                        //已收到信息的人
+                        $received_users = explode(" ", $tmp[0]['received_users']);
+                        $tmpName = User::where('user_id', $tmp[0]['build_id'])->get('name')->first();
+                        $tmp[0]['name'] = $tmpName['name'];
+                        $tmp[0]['noticeType'] = "notice";
+                        $tmp_received = [];
+                        foreach ($received_users as $key => $received_user) {
+                            //如果相等的话表示已提交，不push
+                            if ($received_user != $request->user_id) {
+                                array_push($tmp_received, $received_user);
+                            } else { }
+                        }
+                        if (count($tmp_received) == count($received_users)) {
+                            array_push($noticeList, $tmp[0]);
                         }
                     }
-                    if(count($tmp_received) == count($received_users)){
-                        array_push($noticeList,$tmp[0]);
+                }
+            }
+            foreach ($noticeList as $item) {
+                array_push($result, $item);
+            }
+
+            $len = count($result);
+            for ($k = 0; $k <= $len; $k++) {
+                for ($j = $len - 1; $j > $k; $j--) {
+
+                    if ($result[$j]['created_at']->gt($result[$j - 1]['created_at'])) {
+                        //                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."大<br>";
+                        $temp = $result[$j];
+                        $result[$j] = $result[$j - 1];
+                        $result[$j - 1] = $temp;
+                    } else {
+                        //                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."小<br>";
                     }
                 }
             }
-        }
-        foreach ($noticeList as $item){
-            array_push($result,$item);
-        }
-
-        $len = count($result);
-        for($k = 0 ; $k <= $len ; $k++) {
-            for($j=$len-1;$j>$k;$j--){
-
-                if($result[$j]['created_at']->gt($result[$j-1]['created_at'])){
-//                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."大<br>";
-                    $temp = $result[$j];
-                    $result[$j] = $result[$j-1];
-                    $result[$j-1] = $temp;
-                }else{
-//                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."小<br>";
-                }
-            }
+            $value = Cache::put($cacheName, $result, 60*60*24);
         }
 
         return $result;
@@ -363,86 +398,127 @@ class NoticeController extends Controller
      * @return array
      * @author lj
      * @time 2019-08-29
+     * 加入缓存 有效期一天
+     * @author lj
+     * @time 2019-09-15
      */
     public function getHistoryMessage(Request $request)
     {
-        //传入的参数到月份 如2019-09
-        $startDate = $request->startDate;
-        $endDate = $request->endDate;
+        $cacheName = 'historyMessage'.$request->user_id;
+        if ($value = Cache::get($cacheName)) {
+            return $value;
+        } else {
+            //传入的参数到月份 如2019-09
+            $startDate = $request->startDate;
+            $endDate = $request->endDate;
 
-        $result = [];
+            $result = [];
 
-        $chemicalList = ChemicalsNotice::where([
-            ["user_id_2",$request->user_id],
-            ["receive","!=","0"]
-        ])
-            ->whereDate('updated_at','>=',$startDate)
-            ->whereDate('updated_at','<=',$endDate)
-            ->get();
-        foreach ($chemicalList as $item){
-            $item['noticeType'] = "chemical";
-            array_push($result,$item);
-        }
+            $chemicalList = ChemicalsNotice::where([
+                ["user_id_2", $request->user_id],
+                ["receive", "!=", "0"]
+            ])
+                ->whereDate('updated_at', '>=', $startDate)
+                ->whereDate('updated_at', '<=', $endDate)
+                ->get();
+            foreach ($chemicalList as $item) {
+                $item['noticeType'] = "chemical";
+                //年月日
+                $item['time'] = $item['updated_at']->format('Y-m-d');
+                array_push($result, $item);
+            }
 
-        //被驳回已了解的
-        $chemicaldisagreeList = ChemicalsNotice::where([
-            ["user_id_1",$request->user_id],
-            ["isConfirm_2","0"],
-            ["receive","2"]
-        ])
-            ->whereDate('updated_at','>=',$startDate)
-            ->whereDate('updated_at','<=',$endDate)
-            ->get();
-        foreach($chemicaldisagreeList as $item){
-            $item['noticeType'] = "chemical";
-            array_push($result,$item);
-        }
+            //被驳回已了解的
+            $chemicaldisagreeList = ChemicalsNotice::where([
+                ["user_id_1", $request->user_id],
+                ["isConfirm_2", "0"],
+                ["receive", "2"]
+            ])
+                ->whereDate('updated_at', '>=', $startDate)
+                ->whereDate('updated_at', '<=', $endDate)
+                ->get();
+            foreach ($chemicaldisagreeList as $item) {
+                $item['noticeType'] = "chemical";
+                array_push($result, $item);
+            }
 
-        $tmplist = Notice::all();
-        $users = [];
-        $noticeList = [];
-        foreach ($tmplist as $item){
-            //将0062 0061转为数组
-            $users[$item['id']] = explode(" ", $item['received_users']);
-        }
-        //遍历users
-        $count = 0;
-        foreach( $users as $key=>$user ){
-            for($i = 0 ; $i < count($user) ; $i++){
-                if($user[$i] == $request->user_id){
-                    $tmp = Notice::where('id',$key)
-                        ->whereDate('updated_at','>=',$startDate)
-                        ->whereDate('updated_at','<=',$endDate)
-                        ->get();
-                    if(count($tmp) != 0){
-                        array_push($noticeList,$tmp[0]);
+            $tmplist = Notice::all();
+            $users = [];
+            $noticeList = [];
+            foreach ($tmplist as $item) {
+                //将0062 0061转为数组
+                $users[$item['id']] = explode(" ", $item['received_users']);
+            }
+            //遍历users
+            $count = 0;
+            foreach ($users as $key => $user) {
+                for ($i = 0; $i < count($user); $i++) {
+                    if ($user[$i] == $request->user_id) {
+                        $tmp = Notice::where('id', $key)
+                            ->whereDate('updated_at', '>=', $startDate)
+                            ->whereDate('updated_at', '<=', $endDate)
+                            ->get();
+                        if (count($tmp) != 0) {
+                            array_push($noticeList, $tmp[0]);
+                        }
                     }
                 }
             }
-        }
-        foreach ($noticeList as $item){
-            $item['noticeType'] = "notice";
-            $name = [$item['build_id']];
-            $item['name'] = $this->getNames($name);
-            array_push($result,$item);
-        }
+            foreach ($noticeList as $item) {
+                $item['noticeType'] = "notice";
+                //年月日
+                $item['time'] = $item['updated_at']->format('Y-m-d');
+                $name = [$item['build_id']];
+                $item['name'] = $this->getNames($name);
+                array_push($result, $item);
+            }
 
-        $len = count($result);
-        for($k = 0 ; $k <= $len ; $k++) {
-            for($j=$len-1;$j>$k;$j--){
+            $len = count($result);
+            for ($k = 0; $k <= $len; $k++) {
+                for ($j = $len - 1; $j > $k; $j--) {
 
-                if($result[$j]['updated_at']->gt($result[$j-1]['updated_at'])){
-//                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."大<br>";
-                    $temp = $result[$j];
-                    $result[$j] = $result[$j-1];
-                    $result[$j-1] = $temp;
-                }else{
-//                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."小<br>";
+                    if ($result[$j]['updated_at']->gt($result[$j - 1]['updated_at'])) {
+                        //                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."大<br>";
+                        $temp = $result[$j];
+                        $result[$j] = $result[$j - 1];
+                        $result[$j - 1] = $temp;
+                    } else {
+                        //                    echo $result[$j]['created_at']."比".$result[$j-1]['created_at']."小<br>";
+                    }
                 }
             }
+            $value = Cache::put($cacheName, $result, 60*60*24);
         }
 
         return $result;
+    }
+
+    /**
+     * 清除所有消息的缓存
+     */
+    public function clearAllCache(Request $request)
+    {
+        $user_id = $request->user_id;
+        
+        if (Cache::has('allMessage'.$user_id)) {
+            Cache::forget('allMessage'.$user_id);
+        }else{
+            return "没有这个缓存";
+        }
+    }
+
+    /**
+     * 清除历史消息的缓存
+     */
+    public function clearHistoryCache(Request $request)
+    {
+        $user_id = $request->user_id;
+
+        if (Cache::has('historyMessage'.$user_id)) {
+            Cache::forget('historyMessage'.$user_id);
+        }else{
+            return "没有这个缓存";
+        }
     }
 
     /**
@@ -456,16 +532,16 @@ class NoticeController extends Controller
     {
         $id = $request->id; //notice表的id
         $user = $request->user; //收到通知用户的id
-        $tmp_received_users = Notice::where('id',$id)->get('received_users');
+        $tmp_received_users = Notice::where('id', $id)->get('received_users');
 
         $received_users = $tmp_received_users[0]['received_users'];
-        if($received_users!=null){
-            $received_users = $received_users.' '.$user; // 0001 0002 0003
-        }else{
+        if ($received_users != null) {
+            $received_users = $received_users . ' ' . $user; // 0001 0002 0003
+        } else {
             $received_users = $user;
         }
 
-        Notice::where('id',$id)->update(['received_users' => $received_users]);
+        Notice::where('id', $id)->update(['received_users' => $received_users]);
 
         return '提交成功';
     }
