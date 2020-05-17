cf5d3e3ae975173d30495a98084ce18de3ff2a63
Norsl <43179107+Norsl-s@users.noreply.github.com>
2020-05-17 12:39:00 +0800

    重构电子巡检，提出重复代码
    重构电子巡检，提出重复代码

--- app/Http/Controllers/InspectionController.php
+++ app/Http/Controllers/InspectionController.php
@@ -43,131 +43,6 @@ class InspectionController extends Controller
         }
     }
 
-    /**
-     * 保存电子巡检记录
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \Exception
-     */
-    public function confirmInspection(Request $request)
-    {
-
-        $datetime = new \DateTime;
-        $log = new InspectionLog();
-        $user = User::where("user_id", $request->repair_user)->first();
-        if ($request->type == "chemical") {
-            $chemical = Chemical::where('chemical_id', $request->id)->first();
-            $name = $chemical->name;
-            $content = "工号为:" . $request->repair_user . "(" . $user->name . ")" . "检查了" . $chemical->laboratories->building_name . $chemical->laboratories->classroom_num . "的" . $chemical->laboratories->laboratory_name . "药品编号为:" . $chemical->chemical_id . "的" . $chemical->name . ",时间:" . $datetime->format('Y-m-d H:i:s') . ",检修结果：" . ($chemical->type == "chemical" ? "危化品" : "药品") . $chemical->status . ($request->detail == "undefined" ? '' : ",备注:" . $request->detail);
-            //Log::info($content);
-            if ($request->status == "正常") {
-                $chemical->status = "正常";
-                $chemical->fix_time = $datetime->format('Y-m-d H:i:s');
-                $log->unit_id = $chemical->unit_id;
-                $log->log = $content;
-                $log->save();
-                $chemical->save();
-            } else {
-                $chemical->status = "存在问题";
-                $chemical->fix_time = $datetime->format('Y-m-d H:i:s');
-                $log->unit_id = $chemical->unit_id;
-                $log->log = $content;
-                $log->save();
-                $chemical->save();
-                $this->fixHiddenStatus($request, $name);
-            }
-            return response()->json('检修完成', 200);
-
-        } elseif ($request->type == "equipment") {
-            $equipment = Equipments::where('asset_number', $request->id)->first();
-            $name = $equipment->equipment_name;
-            $content = "工号为:" . $request->repair_user . "(" . $user->name . ")" . "检查了" . $equipment->laboratories->building_name . $equipment->laboratories->classroom_num . "的" . $equipment->laboratories->laboratory_name . "设别编号为:" . $equipment->asset_number . "的" . $equipment->equipment_name . ",时间:" . $datetime->format('Y-m-d H:i:s') . ",检修结果：设备" . $equipment->status . ($request->detail == "undefined" ? '' : ",备注:" . $request->detail);
-            //Log::info($content);
-            if ($request->status == "正常") {
-                $equipment->status = "正常";
-                $equipment->fix_time = $datetime->format('Y-m-d H:i:s');
-                $log->unit_id = $equipment->unit_id;
-                $log->log = $content;
-                $log->save();
-                $equipment->save();
-            } else {
-                $equipment->status = "维修";
-                $equipment->fix_time = $datetime->format('Y-m-d H:i:s');
-                $log->unit_id = $equipment->unit_id;
-                $log->log = $content;
-                $log->save();
-                $equipment->save();
-                $this->fixHiddenStatus($request, $name);
-            }
-
-            return response()->json('检修完成', 200);
-        }
-
-    }
-
-    /**
-     * 修改hidden状态
-     */
-    public function fixHiddenStatus($request, $name)
-    {
-//        Log::info($request);
-
-        //需要上报到问题中
-        if ($request->up == 1) {
-            $hidden = new Hidden();
-            $hidden->user_id = $request->repair_user;
-            $hidden->type = "issue";
-            $hidden->position = $request->position;
-            $hidden->title = $request->position . $name;
-            $hidden->detail = $request->detail;
-            $hidden->image = env('APP_URL') . $request->image;
-            $hidden->unit = $request->unit_id;
-            $hidden->occurrence_time = now();
-            $hidden->number = $request->inspection_id;
-            $hidden->save();
-        }
-        //不需要上报的先查询是否存在这个问题，若存在更新问题状态 删除number
-        if ($request->up == 0) {
-            $hidden_id = Hidden::where('number', $request->inspection_id)
-                ->where('position', $request->position)
-                ->first();
-//                Log::info($hidden_id);
-//            Log::info($hidden_id['id']);
-            if ($hidden_id != null && $request->status == '正常') {
-                //判断改变状态
-                $hidden = Hidden::where('number', $request->inspection_id)->first();
-                $hidden->isSolve = 1;
-                $hidden->number = null;
-                $hidden->save();
-
-                //添加log到数据库
-
-                $name = (new ToolController)->getUsername($request->repair_user);
-                $hiddenLog = new HiddensLog();
-                $hiddenLog->hidden_id = $hidden_id['id'];
-                $hiddenLog->user_name = $name;
-                $hiddenLog->reason = $request->detail;
-                if ($request->image != null) {
-                    $hiddenLog->image = env('APP_URL') . $request->image;
-                    $hiddenLog->isSolve = '1';
-                } else {
-                    $hiddenLog->image = null;
-                }
-                $hiddenLog->save();
-            } else if ($hidden_id != null && $request->status != '正常') {
-                $name = (new ToolController)->getUsername($request->repair_user);
-                $hiddenLog = new HiddensLog();
-                $hiddenLog->hidden_id = $hidden_id['id'];
-                $hiddenLog->user_name = $name;
-                $hiddenLog->reason = "巡检但未处理成功！";
-                $hiddenLog->isSolve = '0';
-                $hiddenLog->image = null;
-                $hiddenLog->save();
-            }
-
-
-        }
-    }
 
     /**
      * 获取电子巡检记录
@@ -197,5 +72,174 @@ class InspectionController extends Controller
             return response()->json(InspectionLog::where('unit_id', $request->unit_id)->orderBy("created_at", 'desc')->get(), 200);
         }
 
+    }
+
+    /**
+     * 提交电子巡检
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function confirmInspection(Request $request)
+    {
+        $datetime = new \DateTime;
+
+        $user = User::where("user_id", $request->repair_user)->first();
+        Log::info($request);
+        if ($request->type == "chemical") {
+            $chemical = Chemical::where('chemical_id', $request->id)->first();
+            $name = $chemical->name;
+            if ($request->status == '存在问题') {
+                if ($request->up == 1) {
+                    //上报问题
+                    $this->createHidden($request, $name);
+                    //修改药品状态为存在问题
+                    $chemical->status = "存在问题";
+                    $chemical->fix_time = $datetime->format('Y-m-d H:i:s');
+                    $chemical->save();
+                    $this->createInspectionLog($request, $chemical, $type = 'chemical');
+                } else if ($request->up == 0) {
+                    //检修过后还是处于存在问题状态
+                    $this->noHandled($request);
+                    $this->createInspectionLog($request, $chemical, $type = 'chemical');
+                }
+            } else if ($request->status == '正常') {
+                if ($request->up == 1) {
+                    Log::info("药品由不正常转为正常");
+                    //修改问题状态
+                    $hidden = Hidden::where('number', $request->inspection_id)->first();
+                    $hidden->isSolve = 1;
+                    $hidden->number = null;
+                    $hidden->save();
+                    $chemical->status = "正常";
+                    $chemical->fix_time = $datetime->format('Y-m-d H:i:s');
+                    $this->createHiddenRecode($request, $hidden['id']);
+                    $this->createInspectionLog($request, $chemical, $type = 'chemical');
+                } else if ($request->up == 0) {
+                    Log::info("药品依旧正常");
+                    $this->createInspectionLog($request, $chemical, $type = 'chemical');
+                }
+
+            }
+        } else if ($request->type == "equipment") {
+            $equipment = Equipments::where('asset_number', $request->id)->first();
+            $name = $equipment->equipment_name;
+            $laboratory = $equipment->laboratories;
+            if ($request->status == '维修') {
+                if ($request->up == 1) {
+                    //上报问题
+                    $this->createHidden($request, $name);
+                    //修改设备状态为维修
+                    $equipment->status = "维修";
+                    $equipment->fix_time = $datetime->format('Y-m-d H:i:s');
+                    $equipment->save();
+                    $this->createInspectionLog($request, $equipment, $type = 'equipment');
+                } else if ($request->up == 0) {
+                    //检修过后还是处于维修状态
+                    $this->noHandled($request);
+                    $this->createInspectionLog($request, $equipment, $type = 'equipment');
+                }
+            } else if ($request->status == '正常') {
+                if ($request->up == 1) {
+                    Log::info("设备由维修转为正常");
+                    $hidden = Hidden::where('number', $request->inspection_id)->first();
+                    $hidden->isSolve = 1;
+                    $hidden->number = null;
+                    $hidden->save();
+                    $equipment->status = "正常";
+                    $equipment->fix_time = $datetime->format('Y-m-d H:i:s');
+                    $equipment->save();
+                    $this->createHiddenRecode($request, $hidden['id']);
+                    $this->createInspectionLog($request, $equipment, $type = 'equipment');
+                } else if ($request->up == 0) {
+                    Log::info("设备依旧正常");
+                    $this->createInspectionLog($request, $equipment, $type = 'equipment');
+                }
+            }
+        }
+        return response()->json('检修完成', 200);
+    }
+
+    /**
+     * 创建隐患问题
+     * @param $request
+     * @param $name
+     */
+    public function createHidden($request, $name)
+    {
+        $hidden = new Hidden();
+        $hidden->user_id = $request->repair_user;
+        $hidden->type = "issue";
+        $hidden->position = $request->position;
+        $hidden->title = $request->position . $name;
+        $hidden->detail = $request->detail;
+        $hidden->image = env('APP_URL') . $request->image;
+        $hidden->unit = $request->unit_id;
+        $hidden->occurrence_time = now();
+        $hidden->number = $request->inspection_id;
+        $hidden->save();
+    }
+
+    /**
+     * 若没处理也需要添加巡检记录
+     * @param $request
+     */
+    public function noHandled($request)
+    {
+        //查询这个问题是否不存在
+        $hidden_id = Hidden::where('number', $request->inspection_id)
+            ->where('position', $request->position)
+            ->first();
+        $name = (new ToolController)->getUsername($request->repair_user);
+        $hiddenLog = new HiddensLog();
+        $hiddenLog->hidden_id = $hidden_id['id'];
+        $hiddenLog->user_name = $name;
+        $hiddenLog->reason = "巡检但未处理成功！";
+        $hiddenLog->isSolve = '0';
+        $hiddenLog->image = null;
+        $hiddenLog->save();
+    }
+
+    /**
+     * 创建问题隐患处理记录
+     * @param $request
+     * @param $hidden_id
+     */
+    public function createHiddenRecode($request, $hidden_id)
+    {
+        $name = (new ToolController)->getUsername($request->repair_user);
+        $hiddenLog = new HiddensLog();
+        $hiddenLog->hidden_id = $hidden_id;
+        $hiddenLog->user_name = $name;
+        $hiddenLog->reason = $request->detail;
+        $hiddenLog->image = env('APP_URL') . $request->image;
+        $hiddenLog->isSolve = '1';
+        $hiddenLog->save();
+    }
+
+    /**
+     * 创建巡检记录
+     * @param $request
+     * @param $data
+     * @param $type
+     * @throws \Exception
+     */
+    public function createInspectionLog($request, $data, $type)
+    {
+        $name = (new ToolController)->getUsername($request->repair_user);
+        $datetime = new \DateTime;
+        $inspectionLog = new InspectionLog();
+        if ($type = 'chemical') {
+            $content = "工号为:" . $request->repair_user . "(" . $name . ")" . "检查了" . $data->laboratories->building_name . $data->laboratories->classroom_num
+                . "的" . $data->laboratories->laboratory_name . "药品编号为:" . $data->chemical_id . "的" . $data->name . ",时间:" . $datetime->format('Y-m-d H:i:s')
+                . ",检修结果：" . ($data->type == "chemical" ? "危化品" : "药品") . $data->status . ($request->detail == "undefined" ? '' : ",备注:" . $request->detail);
+        } else if ($type = 'equipment') {
+            $content = "工号为:" . $request->repair_user . "(" . $name . ")" . "检查了" . $data->laboratories->building_name . $data->laboratories->classroom_num
+                . "的" . $data->laboratories->laboratory_name . "设别编号为:" . $data->asset_number . "的" . $data->equipment_name . ",时间:"
+                . $datetime->format('Y-m-d H:i:s') . ",检修结果：设备" . $data->status . ($request->detail == "undefined" ? '' : ",备注:" . $request->detail);
+        }
+        $inspectionLog->unit_id = $data->unit_id;
+        $inspectionLog->log = $content;
+        $inspectionLog->save();
     }
 }
